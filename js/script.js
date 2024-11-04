acbc6d9f9d38357c1f0e0bf081fc7cc725364cc4
pedrinho-devv <pedrodessousakiske@gmail.com>
2024-11-04 08:06:56 -0300

    update em js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const meubnt = document.querySelector(".ham-menu");
 const navBar = document.querySelector(".nav-bar");
 
 function handleClick() {
-    navBar.classList.toggle("active"); // Adiciona ou remove a classe "active" ao menu
+    navBar.classList.toggle("active"); 
 }
 
-// Adiciona o evento de clique no botão do menu
+
 meubnt.addEventListener("click", handleClick);
 
-// Fecha o menu quando um link é clicado
+
 const menuLinks = document.querySelectorAll(".menu_links li a");
 menuLinks.forEach(link => {
     link.addEventListener("click", () => {
@@ -16,11 +16,11 @@ menuLinks.forEach(link => {
     });
 });
 
-// Fecha o menu se o usuário clicar fora dele
+
 document.addEventListener("click", function(event) {
-    // Verifica se o clique foi fora do menu e do botão
+
     if (!navBar.contains(event.target) && !meubnt.contains(event.target)) {
-        navBar.classList.remove("active"); // Remove a classe "active"
+        navBar.classList.remove("active"); 
     }
 });
 
@@ -30,7 +30,7 @@ document.addEventListener("click", function(event) {
 let slideIndex = 0;
 showSlides(slideIndex);
 
-// Função para avançar ou retroceder slides
+
 function moveSlide(n) {
     showSlides(slideIndex += n);
 }
@@ -39,21 +39,20 @@ function moveSlide(n) {
 function showSlides(n) {
     const slides = document.querySelectorAll('.carousel-item');
     
-    // Volta ao primeiro slide se o índice exceder o número de slides
+    
     if (n >= slides.length) { 
         slideIndex = 0;
     }
     
-    // Vai ao último slide se o índice for menor que 0
+    
     if (n < 0) { 
         slideIndex = slides.length - 1;
     }
 
-    // Esconde todos os slides
     slides.forEach((slide) => {
         slide.style.display = "none";
     });
 
-    // Exibe o slide ativo
+ 
     slides[slideIndex].style.display = "block";
 }
